f160be987acbc31ec3aa8cad326af36bc2352a18
Jay stones <Joelpaul345@gmail.com>
2023-08-31 13:47:25 +0100

    :wrench: Routes

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import startServer from './libs/boot';
-import injectRoutes from './routes';
+import Routes from './routes';
 import injectMiddlewares from './libs/middlewares';
 
 const server = express();
 
 injectMiddlewares(server);
-injectRoutes(server);
+Routes(server);
 startServer(server);
 
 export default server;
