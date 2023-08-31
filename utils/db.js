053af1bd682c1c0a1e44d827b2affb0c3ed54cf1
Jay stones <Joelpaul345@gmail.com>
2023-08-31 23:22:48 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,65 +1,38 @@
 import { MongoClient } from 'mongodb';
 import Collection from 'mongodb/lib/collection';
 
+const HOST = process.env.DB_HOST || 'localhost';
+const PORT = process.env.DB_PORT || 27017;
+const DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${HOST}:${PORT}`;
+
 class DBClient {
   constructor() {
-    const dbHost = process.env.DB_HOST || 'localhost';
-    const dbPort = process.env.DB_PORT || 27017;
-    const dbName = process.env.DB_DATABASE || 'files_manager';
-
-    this.uri = `mongodb://${dbHost}:${dbPort}`;
-    this.client = new MongoClient(this.uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    this.db = null;
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+    this.client.connect().then(() => {
+      this.db = this.client.db(`${DATABASE}`);
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
-  async connect() {
-    try {
-      await this.client.connect();
-      this.db = this.client.db(process.env.DB_DATABASE || 'files_manager');
-      console.log("Connected to MongoDB");
-    } catch (error) {
-      console.error("Error connecting to MongoDB:", error);
-    }
-  }
-
-  async isAlive() {
+  isAlive() {
     return this.client.isConnected();
   }
 
   async nbUsers() {
-    try {
-      if (!this.db) {
-        throw new Error("Database connection not established");
-      }
-      const usersCollection = this.db.collection('users');
-      const count = await usersCollection.countDocuments();
-      return count;
-    } catch (error) {
-      console.error("Error counting users:", error);
-      return -1;
-    }
+    const users = this.db.collection('users');
+    const usersNum = await users.countDocuments();
+    return usersNum;
   }
 
   async nbFiles() {
-    try {
-      if (!this.db) {
-        throw new Error("Database connection not established");
-      }
-      const filesCollection = this.db.collection('files');
-      const count = await filesCollection.countDocuments();
-      return count;
-    } catch (error) {
-      console.error("Error counting files:", error);
-      return -1;
-    }
-  }
-
-  async close() {
-    await this.client.close();
-    console.log("Closed MongoDB connection");
+    const files = this.db.collection('files');
+    const filesNum = await files.countDocuments();
+    return filesNum;
   }
 }
 
 const dbClient = new DBClient();
-
+module.exports = dbClient;
 module.exports = dbClient;
